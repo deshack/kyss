056c1e8027c83f61856e6067c1d076801ffd526f
Mattia Migliorini <deshack@ubuntu.com>
2014-08-25 14:38:30 +0200

    Fix #33 Reduce sidebar width

--- views/sidebar.php
+++ views/sidebar.php
@@ -8,26 +8,28 @@
  */
 ?>
 
-<section id="sidebar" class="medium-3 columns">
+<section id="sidebar" class="medium-2 columns">
 	<ul class="side-nav">
 		<li><a href="<?php echo get_option( 'siteurl' ); ?>">Home</a></li>
 		<li><a href="<?php echo get_option( 'siteurl' ); ?>/users.php">Utenti</a></li>
 		<li><a href="<?php echo get_option( 'siteurl' ); ?>/events.php">Eventi</a>
-			<ul><a href="<?php echo get_option( 'siteurl' ); ?>/meetings.php">Riunioni</a></ul>
-			<ul><a href="<?php echo get_option( 'siteurl' ); ?>/courses.php">Corsi</a></ul>
-			<ul><a href="<?php echo get_option( 'siteurl' ); ?>/other-events.php">Altri</a></ul>
+			<ul>
+				<li><a href="<?php echo get_option( 'siteurl' ); ?>/meetings.php">Riunioni</a></li>
+				<li><a href="<?php echo get_option( 'siteurl' ); ?>/courses.php">Corsi</a></li>
+				<li><a href="<?php echo get_option( 'siteurl' ); ?>/other-events.php">Altri</a></li>
+			</ul>
 		</li>
 		<li><a href="<?php echo get_option( 'siteurl' ); ?>/talks.php">Documenti</a></li>
 		<li><a href="<?php echo get_option( 'siteurl' ); ?>/documents.php">Documenti</a>
-			<ul><a href="<?php echo get_option( 'siteurl' ); ?>/practices.php">Pratiche</a></ul>
-			<ul><a href="<?php echo get_option( 'siteurl' ); ?>/reports.php">Verbali</a></ul>
-			<ul><a href="<?php echo get_option( 'siteurl' ); ?>/budgets.php">Bilanci</a></ul>
+			<ul>
+				<li><a href="<?php echo get_option( 'siteurl' ); ?>/practices.php">Pratiche</a></li>
+				<li><a href="<?php echo get_option( 'siteurl' ); ?>/reports.php">Verbali</a></li>
+				<li><a href="<?php echo get_option( 'siteurl' ); ?>/budgets.php">Bilanci</a></li>
+			</ul>
 		</li>
 		<li><a href="<?php echo get_option( 'siteurl' ); ?>/movements.php">Movimenti</a></li>
 		<li><a href="<?php echo get_option( 'siteurl' ); ?>/offices.php">Cariche</a></li>
-		<li class="divider"></li>
-		<li class="active"><a href="#">Link active</a></li>
 	</ul>
 </section><!-- #sidebar -->
 
-<main id="content" class="medium-9 columns" role="main">
+<main id="content" class="medium-10 columns" role="main">
