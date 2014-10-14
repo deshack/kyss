f0f724c394f95f7ea65e48da1602e60b8ad387ec
Mattia Migliorini <migliorini.vb.italia@gmail.com>
2014-10-14 16:49:09 +0200

    Complete first implementation of KYSS class

--- core/inc/kyss.php
+++ core/inc/kyss.php
@@ -21,7 +21,7 @@ class KYSS {
 	 *
 	 * @since  1.0.0
 	 * @access private
-	 * @var string
+	 * @var string|object
 	 */
 	private $controller;
 
@@ -49,13 +49,10 @@ class KYSS {
 	 * Analyze the URL elements and calls the according controller/method
 	 * or the fallback.
 	 *
-	 * This is set as private because we want a singleton instance of
-	 * this class.
-	 *
-	 * @since  0.15.0
-	 * @access private
+	 * @since  1.0.0
+	 * @access public
 	 */
-	private function __construct() {
+	public function __construct() {
 		// Create array with URL parts in $url.
 		$this->splitUrl();
 
@@ -68,8 +65,41 @@ class KYSS {
 			// Check for method.
 			if ( method_exists( $this->controller, $this->action ) ) {
 				// Call the method and pass the arguments to it.
-				// if ( isset( $this->))
+				if ( isset( $this->parameters ) )
+					call_user_func_array( array( $this->controller, {$this->action} ), $this->parameters );
+				else // Call the method without parameters.
+					$this->controller->{$this->action}();
+			} else {
+				// Fallback: call the index() method of the selected controller.
+				$this->controller->index();
 			}
+		} else {
+			// Invalid URL, show home/index.
+			// TODO: Show Error 404 Page instead.
+			require ABSPATH . 'core/controller/home.php';
+			$home = new Home();
+			$home->index();
 		}
+	}
+
+	/**
+	 * Retrieve and split the URL.
+	 *
+	 * @since 1.0.0
+	 * @access private
+	 */
+	private function splitUrl() {
+		$url = trim( $_SERVER['REQUEST_URI'], '/' );
+		$url = filter_var( $url, FILTER_SANITIZE_URL );
+		$url = explode( '/', $url );
+
+		if ( empty( $url ) )
+			return;
+
+		// Put URL parts into according properties.
+		// `array_shift()` returns NULL if `$url` is empty.
+		$this->controller = array_shift( $url );
+		$this->action = array_shift( $url );
+		$this->parameters = (!empty( $url ) ? $url : null);
 	}
 }
